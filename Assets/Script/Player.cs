9997755aaeb4d93462480bac1f0b2b4793e60eae
prostoAccaynt <77786237+prostoAccaynt@users.noreply.github.com>
2021-02-11 16:31:31 +0300

    Добавил управление и новую текстуру земли

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float speed = 200f;
+        public Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>(); 
+    
+        
+    }
+
+    
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+    rb.MovePosition(rb.position + Vector2.right * moveX * speed * Time.deltaTime);
+    
+        
+    }
+}
