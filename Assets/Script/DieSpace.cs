4f4fa3f2a40ee14eb9993bdb8d004c8839581b23
prostoAccaynt <77786237+prostoAccaynt@users.noreply.github.com>
2021-02-18 16:19:33 +0300

    Доделал игру надо делать проект

--- Assets/Script/DieSpace.cs
+++ Assets/Script/DieSpace.cs
@@ -1,11 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
 
 public class DieSpace : MonoBehaviour
 {
-    void OnTriggerEnter(Collider)
+    public GameObject respawn;
+    void OnTriggerEnter2D(Collider2D other)
     {
-
+        if (other.tag == "Chel")
+        {
+            Debug.Log("asdfsaf");
+            other.transform.position = respawn.transform.position;
+        }
     }
 }
--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -7,6 +7,8 @@ public class Player : MonoBehaviour
 
     public float speed = 200f;
         public Rigidbody2D rb;
+
+    private bool faceRight = true;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>(); 
@@ -23,5 +25,15 @@ public class Player : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Space))
             rb.AddForce(Vector2.up * 10000);
+
+        if (moveX > 0 && !faceRight)
+            flip();
+        else if (moveX < 0 && faceRight)
+            flip();
+    }
+      void flip ()
+    {
+        faceRight = !faceRight;
+        transform.localScale = new Vector3 (transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
     }
 }
