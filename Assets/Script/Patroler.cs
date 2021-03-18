48727d3639bc42f57ab87a65b2a57d5d93c8c80c
prostoAccaynt <prohor.sytov@yandex.ru>
2021-03-18 16:20:50 +0300

    Доделал бота начал делать шкалу здоровья

--- Assets/Script/Patroler.cs
+++ Assets/Script/Patroler.cs
@@ -10,6 +10,12 @@ public class Patroler : MonoBehaviour
     public Transform point;
     bool moveingRight;
 
+    Transform Chel;
+    public float stoppingDistance;
+
+    bool chill = false;
+    bool angry = false;
+    bool goBack = false;
 
 
 
@@ -17,40 +23,71 @@ public class Patroler : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Chel = GameObject.FindGameObjectWithTag("Chel").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Vector2.Distance(transform.position, point.position) < positionOfPatrol)
+        if (Vector2.Distance(transform.position, point.position) < positionOfPatrol && angry == false)
+        {
+            chill = true;
+        }
+        if (Vector2.Distance(transform.position, Chel.position) < stoppingDistance)
+        {
+            angry = true;
+            chill = false;
+            goBack = false;
+        }
+        if (Vector2.Distance(transform.position, Chel.position) > stoppingDistance)
+        {
+            goBack = true;
+            angry = false;
+        }
+
+        if (chill == true)
         {
             Chill();
         }
+        else if (angry == true)
+        {
+            Angry();
+        }
+        else if (goBack == true)
+        {
+            GoBack();
+        }
+
     }
     void Chill()
     {
         if (transform.position.x > point.position.x + positionOfPatrol)
         {
-            moveingRight = true;
+            moveingRight = false;
         }
         else if (transform.position.x < point.position.x + - positionOfPatrol)
         {
-            moveingRight = false;
+            moveingRight = true;
         }
 
         if (moveingRight)
         {
             transform.position = new Vector2(transform.position.x + speed * Time.deltaTime, transform.position.y);
         }
+        else
+        {
+            transform.position = new Vector2(transform.position.x - speed * Time.deltaTime, transform.position.y); 
+        }
 
     }
     void Angry()
     {
-
+        transform.position = Vector2.MoveTowards(transform.position, Chel.position, speed * Time.deltaTime);
+        speed = 9;
     }
     void GoBack()
     {
-
+        transform.position = Vector2.MoveTowards(transform.position, point.position, speed * Time.deltaTime);
     }
 }
+
