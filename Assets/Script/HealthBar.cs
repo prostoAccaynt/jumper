12545d1c432fd9ddcb65a096445ccc66a3eebf34
prostoAccaynt <prohor.sytov@yandex.ru>
2021-04-02 16:30:35 +0300

    Сделал шкалу здоровья начал подделывать игрока

--- Assets/Script/HealthBar.cs
+++ Assets/Script/HealthBar.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public Slider slider;
+    public Gradient gradient;
+
+    public void SetMaxHealth(int health)
     {
-        
+        slider.maxValue = health;
+        slider.value = health;
+    }
+    
+
+    public void SetHealth(int health)
+    {
+        slider.value = health;
     }
 }
--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -4,19 +4,31 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-
+    public int maxHealth = 100;
+    public int currentHealth;
+    public HealthBar healthBar;
     public float speed = 200f;
         public Rigidbody2D rb;
+    private Animation anim;
+    private float moveInput;
+    public bool facingRight = true;
 
-    private bool faceRight = true;
+    private bool isGrounded;
+
+    
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>(); 
-    
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
+        healthBar.SetHealth(maxHealth);
         
     }
 
-    
+    private void FixedUpdate()
+    {
+        
+    }
     void Update()
     {
         float moveX = Input.GetAxis("Horizontal");
@@ -30,10 +42,25 @@ public class Player : MonoBehaviour
             flip();
         else if (moveX < 0 && faceRight)
             flip();
+        if (moveInput == 0)
+        {
+            anim.SetBool("isRunning", false);
+        }
+        else
+        {
+            anim.SetBool("isRunning", true);
+        }
     }
       void flip ()
     {
         faceRight = !faceRight;
         transform.localScale = new Vector3 (transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        healthBar.SetHealth(currentHealth);
     }
 }
