aaa2c28c5dc0f8e69d8129b467d28c24297ee3fd
prostoAccaynt <77786237+prostoAccaynt@users.noreply.github.com>
2021-04-08 16:30:52 +0300

    Сидел в плеере. Flip!

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -4,63 +4,144 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public int maxHealth = 100;
-    public int currentHealth;
-    public HealthBar healthBar;
     public float speed = 200f;
         public Rigidbody2D rb;
     private Animation anim;
+    public bool faceRight = true;
+    public  flip;
+
+    public float jampForce;
     private float moveInput;
-    public bool facingRight = true;
 
     private bool isGrounded;
-
+    public Transform feetPos;
+    public float checkRadius;
+    public LayerMask whatIGround;
     
-    void Start()
-    {
-        anim = GetComponent<Animator>();
-        rb = GetComponent<Rigidbody2D>();
-        currentHealth = maxHealth;
-        healthBar.SetHealth(maxHealth);
+
+    public int maxHealth = 100;
+    public int currentHealth;
+    public HealthBar healthBar;
+
+    //anim = GetComponent<Animator>();
+     //   rb = GetComponent<Rigidbody2D>();
         
+       // healthBar.SetHealth(maxHealth);
+
+
+
+
+
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
     }
 
-    private void FixedUpdate()
-    {
-        
-    }
-    void Update()
+    private void fixedUpdate()
     {
         float moveX = Input.GetAxis("Horizontal");
-    rb.MovePosition(rb.position + Vector2.right * moveX * speed * Time.deltaTime);
+        rb.MovePosition(rb.position + Vector2.right * moveX * speed * Time.deltaTime);
 
 
         if (Input.GetKeyDown(KeyCode.Space))
-            rb.AddForce(Vector2.up * 10000);
-
+          rb.AddForce(Vector2.up * 10000);
+        
         if (moveX > 0 && !faceRight)
             flip();
         else if (moveX < 0 && faceRight)
             flip();
+
         if (moveInput == 0)
         {
-            anim.SetBool("isRunning", false);
+           anim.SetBool("isRunning", false);
         }
-        else
-        {
-            anim.SetBool("isRunning", true);
-        }
-    }
-      void flip ()
-    {
-        faceRight = !faceRight;
-        transform.localScale = new Vector3 (transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
-    }
+      //  else
+      // /{
+      //   anim.SetBool("isRunning", true);
+      //  }
 
-    void TakeDamage(int damage)
+
+         //moveInput = Input.GetAxis("Horizontal");
+        //rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+    }
+    private void Update()
+    {
+        
+    }
+     // void flip ()
+   // {
+    //    facingRight = !facingRight;
+     //   Vector3 Scaler = transform.localScale;
+    //   Scaler.x *= -1;
+        //transform.localScale = new Vector3 (transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+    
+
+     void TakeDamage(int damage)
     {
         currentHealth -= damage;
 
         healthBar.SetHealth(currentHealth);
     }
+
 }
+
+
+
+
+
+
+
+
+//isGrounded = Phisics2D.OverlapCircle(feetPos.position, checkedRadius, whatIGround);
+
+//if (isGrounded == true && moveInput.GetKeyDown(KeyCode.Space))
+//{
+   // rb.velocity = Vector2.up * jampForce;
+//}
+
+
+
+//float moveX = Input.GetAxis("Horizontal");
+//rb.MovePosition(rb.position + Vector2.right * moveX * speed * Time.deltaTime);
+
+
+//if (Input.GetKeyDown(KeyCode.Space))
+//  rb.AddForce(Vector2.up * 10000);
+
+//if (moveX > 0 && !faceRight)
+//   flip();
+//else if (moveX < 0 && faceRight)
+//   flip();
+//if (moveInput == 0)
+//{
+//   anim.SetBool("isRunning", false);
+//}
+//else
+//{
+// anim.SetBool("isRunning", true);
+//}
+//  }
+
+
+
+
+// if (Input.GetKeyDown(KeyCode.Space))
+//    rb.AddForce(Vector2.up * 10000);
+
+// if (moveX > 0 && !faceRight)
+//    flip();
+// else if (moveX < 0 && faceRight)
+//    flip();
+// if (moveInput == 0)
+// {
+//  anim.SetBool("isRunning", false);
+//}
+//else
+//{
+//   anim.SetBool("isRunning", true);
+//}
