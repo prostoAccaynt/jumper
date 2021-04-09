cd9e34ed09cb1b65eb766ad7a3385957105d3cb2
prostoAccaynt <prohor.sytov@yandex.ru>
2021-04-09 16:27:17 +0300

    Начал делать пулю

---
+++ Assets/Gun.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+
+    public float offset;
+    public GameObject bullet;
+    public Transform shotPoint;
+
+    private float timeBtwShots;
+    public float startTimeBtwShots;
+    void Update()
+    {
+        Vector3 differnce = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float rotz = Mathf.Atan2(differnce.y, differnce.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotz + offset);
+
+        if (timeBtwShots <= 0)
+        {
+
+            if (Input.GetMouseButton(0))
+            {
+                Instantiate(bullet, shotPoint.position, transform.rotation);
+                timeBtwShots = startTimeBtwShots;
+            }
+        }
+        else
+        {
+            timeBtwShots -= Time.deltaTime;
+        }
+    }
+}
--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed = 200f;
-        public Rigidbody2D rb;
+    private  Rigidbody2D rb;
     private Animation anim;
     public bool faceRight = true;
-    public  flip;
+    
 
     public float jampForce;
     private float moveInput;
@@ -16,7 +16,7 @@ public class Player : MonoBehaviour
     private bool isGrounded;
     public Transform feetPos;
     public float checkRadius;
-    public LayerMask whatIGround;
+    public LayerMask whatIsGround;
     
 
     public int maxHealth = 100;
@@ -42,46 +42,62 @@ public class Player : MonoBehaviour
         healthBar.SetMaxHealth(maxHealth);
     }
 
-    private void fixedUpdate()
+    void Update()
     {
         float moveX = Input.GetAxis("Horizontal");
         rb.MovePosition(rb.position + Vector2.right * moveX * speed * Time.deltaTime);
 
-
+        //прыжок
         if (Input.GetKeyDown(KeyCode.Space))
-          rb.AddForce(Vector2.up * 10000);
-        
-        if (moveX > 0 && !faceRight)
-            flip();
+             rb.AddForce(Vector2.up * 10000);
+
+            if (moveX > 0 && !faceRight)
+            Flip();
         else if (moveX < 0 && faceRight)
-            flip();
-
-        if (moveInput == 0)
-        {
-           anim.SetBool("isRunning", false);
-        }
-      //  else
-      // /{
-      //   anim.SetBool("isRunning", true);
-      //  }
-
-
-         //moveInput = Input.GetAxis("Horizontal");
-        //rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
-    }
-    private void Update()
+            Flip();
+    } 
+    void flip ()
     {
-        
+        faceRight = !faceRight;
     }
-     // void flip ()
-   // {
-    //    facingRight = !facingRight;
-     //   Vector3 Scaler = transform.localScale;
-    //   Scaler.x *= -1;
-        //transform.localScale = new Vector3 (transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
-    
+    private void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
 
-     void TakeDamage(int damage)
+        if (isGrounded == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = Vector2.up * jampForce;
+        }
+    }
+
+
+    //  else
+    // /{
+    //   anim.SetBool("isRunning", true);
+    //  }
+
+
+    //moveInput = Input.GetAxis("Horizontal");
+    //rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
+    void Flip ()
+    {
+        faceRight = !faceRight;
+           Vector3 Scaler = transform.localScale;
+           Scaler.x *= -1;
+        transform.localScale = new Vector3 (transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+
+
+    }
+    // void flip ()
+    // {
+    //    facingRight = !facingRight;
+    //   Vector3 Scaler = transform.localScale;
+    //   Scaler.x *= -1;
+    //transform.localScale = new Vector3 (transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+
+
+    void TakeDamage(int damage)
     {
         currentHealth -= damage;
 
